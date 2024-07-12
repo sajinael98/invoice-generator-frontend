45ab6c8786bc79d75da03e328d905dd73d715a0e
sajinale98 <sajinael00@gmail.com>
2024-07-13 02:08:29 +0300

    add no data component in TableBody

--- src/components/Table/TableBody/TableBody.tsx
+++ src/components/Table/TableBody/TableBody.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { TableBodyProps } from './TableBody.d'
 import { flexRender } from '@tanstack/react-table'
+import { Flex, Stack, Text, ThemeIcon } from '@mantine/core'
+import { IconDatabase } from '@tabler/icons-react'
 
 const TableBody = ({ rowModel }: TableBodyProps) => {
     return (
@@ -14,6 +16,14 @@ const TableBody = ({ rowModel }: TableBodyProps) => {
                     ))}
                 </tr>
             ))}
+            {rowModel().rows.length == 0 && <Flex w='100%' h={400} justify='center' align='center'>
+                <Stack align='center'>
+                    <ThemeIcon size='xl' variant='light'>
+                        <IconDatabase />
+                    </ThemeIcon>
+                    <Text fw={700}>No Data</Text>
+                </Stack>
+            </Flex>}
         </tbody>
     )
 }
